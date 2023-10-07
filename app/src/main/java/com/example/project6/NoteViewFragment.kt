cc77b40dfab7478eecaa6121dcc637333e210908
toastyman231 <jmorgenstern15@gmail.com>
2023-10-07 14:05:28 -0400

    Added nav graph and split main activity off into it's own fragment.

---
+++ app/src/main/java/com/example/project6/NoteViewFragment.kt
@@ -0,0 +1,22 @@
+package com.example.project6
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [NoteViewFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class NoteViewFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_view, container, false)
+    }
+}
