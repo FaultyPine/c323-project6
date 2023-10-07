6ba2c9601fed731c5633b242fd663234af5af9ce
faultypine <gclark1013@gmail.com>
2023-10-07 17:05:40 -0400

    delete note confirmation

--- app/src/main/java/com/example/project6/NoteViewFragment.kt
+++ app/src/main/java/com/example/project6/NoteViewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -35,8 +36,13 @@ class NoteViewFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = ItemAdapter(
-            { noteId: Long -> viewModel.onNoteClicked(noteId) },
-            {noteId: Long -> viewModel.deleteNote(noteId) }
+            { noteId: Long -> run {
+                viewModel.onNoteClicked(noteId)
+            }},
+            { noteId: Long -> run {
+                val dialog = NoteDeleteDialogFragment(noteId)
+                dialog.show(childFragmentManager, NoteDeleteDialogFragment.TAG)
+            }}
         )
         binding.itemRecyclerView.adapter = adapter
 
